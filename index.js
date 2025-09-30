faa7bbc6c2e595e35272623de4ef832519997065
HarshitaBrandshow <harshita.trainee@brandshow.in>
2025-09-30 15:24:24 +0530

    mongo Db Connection added

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-// index.js
 require("dotenv").config();
 const express = require("express");
+const db = require("./mongoConnection"); // aapka mongo connection file
+
 const app = express();
-
-
-
 
 // Middleware
 app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Hello, Node.js Project!");
+  res.send("Hello, Node.js Project with MongoDB!");
 });
 
 // Start Server
